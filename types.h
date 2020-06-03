bb2dd10a7f28944025f1a6ecdb56a66f49f69351
Mina Tahaei <minatahaei@protonmail.com>
2020-06-03 20:40:43 +0430

    add main project struct

--- types.h
+++ types.h
@@ -2,3 +2,10 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+
+//Main Project Struct
+struct proc_info
+{
+  int pid;
+  int memsize;
+}
