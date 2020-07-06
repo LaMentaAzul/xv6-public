4dd7b2616866e5a9cb9eb94bcfc3e2df646620a5
Mina Tahaei <minatahaei@protonmail.com>
2020-07-06 13:43:11 +0430

    define test file

---
+++ test.c
@@ -0,0 +1,35 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    int pid;
+    int childpid, wtime , rtime;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf(1, "failed\n");
+        exit();
+    }
+
+    if (pid == 0)
+    {
+	exec(argv[1], argv);
+        printf(1, "child created %d\n", getpid(), argv[1]);
+    }
+
+    else
+    {
+        for (int i = 0; i < 1000; i++) //delay for waiting for child
+        {
+            printf(1, "%d", i);
+        }
+        child_pid = waitx(&wtime, &rtime);
+
+        printf(1, "\n wait time : %d , RunTime : %d childpid : %d\n", wtime, rtime , childpid);
+    }
+    exit();
+} 
